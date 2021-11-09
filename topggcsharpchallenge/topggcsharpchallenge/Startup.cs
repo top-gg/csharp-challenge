2df4dcf235f1f7fc2c60bf8ef6af9deb1a46c751
Petar Parushev <petergparushev@gmail.com>
2021-11-09 16:38:25 +0200

    add swagger

--- topggcsharpchallenge/topggcsharpchallenge/Startup.cs
+++ topggcsharpchallenge/topggcsharpchallenge/Startup.cs
@@ -1,8 +1,11 @@
+using System;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace topggcsharpchallenge
 {
@@ -19,6 +22,22 @@ namespace topggcsharpchallenge
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Earthquake API",
+                    Version = "v1",
+                    Description = "Provides information on earthquakes.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Petar Parushev",
+                        Email = "petergparushev@gmail.com",
+                        Url = new Uri("https://top.gg/"),
+                    },
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,6 +47,15 @@ namespace topggcsharpchallenge
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Earthquake API V1");
+
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
