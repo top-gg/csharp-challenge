704cfb49ee6941887b0b0ed213dc93b7428ac22f
Petar Parushev <petergparushev@gmail.com>
2021-11-09 17:31:53 +0200

    remove unneeded controller and model; add earthquake controller;

---
+++ topggcsharpchallenge/topggcsharpchallenge/Controllers/EearthquakeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace topggcsharpchallenge.Controllers
+{
+    [ApiController]
+    [Route("earthquakes")]
+    public class EearthquakeController : ControllerBase
+    {
+    }
+}
